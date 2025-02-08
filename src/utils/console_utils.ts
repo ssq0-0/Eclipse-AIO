584ed14340ec11cf97b3a3756c3a1b19c0f114e4
ssq0-0 <snikers1kitkat2@gmail.com>
2025-02-08 15:13:04 +0300

    Minor updates

--- src/utils/console_utils.ts
+++ src/utils/console_utils.ts
@@ -12,18 +12,6 @@ async function promptSelection(message: string, options: string[]): Promise<stri
     return selected;
 }
 
-async function handleSubMenu(menuName: string, subMenu: string[]): Promise<string> {
-    while (true) {
-        const selected = await promptSelection(`Choose ${menuName} sub-module:`, subMenu);
-
-        if (selected === "Back") {
-            return "";
-        }
-
-        return selected;
-    }
-}
-
 export async function userChoice(): Promise<string> {
     const mainMenu = [
         "1. Solar",
@@ -31,29 +19,8 @@ export async function userChoice(): Promise<string> {
         "3. Lifinity",
         "4. Underdog",
         "5. Relay",
-        // "2. Ionic",
-        // "3. Relay",
-        // "4. Portal",
-        // "5. BalanceCheck",
-        // "6. Wrap_Unwrap",
         "0. Exit",
     ];
-
-    // const subMenus: { [key: string]: string[] } = {
-    //     "Ionic": [
-    //         "1. Ionic71Supply",
-    //         "2. Ionic15Borrow",
-    //         "3. IonicWithdrawAll",
-    //         "4. IonicRepayAll",
-    //         "0. Back",
-    //     ],
-    //     "Portal": [
-    //         "1. Checker",
-    //         "2. Portal_daily_check",
-    //         "3. Portal_main_tasks",
-    //         "0. Back",
-    //     ],
-    // };
 
     const rgx = /^\d+\.\s*/;
 
@@ -67,17 +34,7 @@ export async function userChoice(): Promise<string> {
             case "Lifinity":
             case "Underdog":
             case "Relay":
-            // case "Relay":
-            // case "BalanceCheck":
-            // case "Wrap_Unwrap":
                 return selected;
-            // case "Ionic":
-            // case "Portal":
-            //     const subSelected = await handleSubMenu(selected, subMenus[selected]);
-            //     if (subSelected !== "") {
-            //         return subSelected;
-            //     }
-            //     break;
             case "Exit":
                 console.log("Exiting program.");
                 return "";
